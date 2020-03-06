bf2b1695a9a0c1416e6774a6a8223a17501fa106
Tisdale Fry <tisdalefry@gmail.com>
2020-03-06 01:32:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,8 @@ end
 def get_japanese_emoticon(file_path, jap_emo)
    binding.pry
   result = load_library(file_path)
-  result['get_emoticon'] do |emo, trans|
-    trans do |usa, jap|
+  result['get_emoticon'].each do |emo, trans|
+    trans.each do |usa, jap|
       jap_emo = usa[jap]
     end
   end
