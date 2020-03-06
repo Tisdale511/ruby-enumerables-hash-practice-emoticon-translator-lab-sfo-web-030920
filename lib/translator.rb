d03b41b6d857113eee54ad88f920db9488fd436b
Tisdale Fry <tisdalefry@gmail.com>
2020-03-06 01:11:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,28 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |name, emoji|
+    hash['get_meaning'][emoji[1]] = name
+    hash['get_emoticon'][emoji[0]] = emoji[1]
+    
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, jap_emo)
+  result = load_library(file_path)
+  result['get_emoticon'] do |usa, jap|
+    
+  end
+    binding.pry
+  jap_emo
 end
 
 def get_english_meaning
