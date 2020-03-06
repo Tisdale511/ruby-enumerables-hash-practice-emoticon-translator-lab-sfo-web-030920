5b228f8386caf172c2056f7b007891cdeeaeec52
Tisdale Fry <tisdalefry@gmail.com>
2020-03-06 01:45:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,8 @@ end
 
 def get_english_meaning(file_path, jap_emo)
   result = load_library(file_path)
-  result['get_emoticon'].each do |usa, jap|
+  binding.pry
+  result['get_meaning'].each do |usa, jap|
     if jap_emo == jap
       return usa
     end
